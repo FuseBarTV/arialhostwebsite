01d1203bc31b02ce0851bd37629bf4b80d917953
FuseBarTV <jordansworld98@outlook.com>
2021-07-04 17:49:17 +1000

    commit

---
+++ template-parts/footer-styles.php
@@ -0,0 +1,31 @@
+<!-- footer styles -->
+
+<style>.u-footer {
+  background-image: none;
+}
+.u-footer .u-sheet-1 {
+  min-height: 143px;
+}
+.u-footer .u-text-1 {
+  width: 417px;
+  margin: 49px auto 0;
+}
+.u-footer .u-btn-1 {
+  margin: -45px auto 49px 224px;
+}
+@media (max-width: 1199px) {
+  .u-footer .u-btn-1 {
+    margin-top: -45px;
+    margin-left: 24px;
+  }
+}
+@media (max-width: 991px) {
+  .u-footer .u-btn-1 {
+    margin-left: 0;
+  }
+}
+@media (max-width: 575px) {
+  .u-footer .u-text-1 {
+    width: 340px;
+  }
+}</style>
---
+++ template-parts/header-styles.php
@@ -0,0 +1,65 @@
+<!-- header styles -->
+
+<link id="u-google-font" rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i|Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i">
+<style>.u-header {
+  background-image: none;
+}
+.u-header .u-sheet-1 {
+  min-height: 95px;
+}
+.u-header .u-image-1 {
+  margin: 23px auto 0 0;
+}
+.u-header .u-logo-image-1 {
+  max-width: 64px;
+  max-height: 64px;
+}
+.u-header .u-menu-1 {
+  margin: -32px 0 0 auto;
+}
+.u-header .u-nav-1 {
+  font-size: 1rem;
+  letter-spacing: 0px;
+}
+.u-block-6cb4-21 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+.u-header .u-nav-2 {
+  font-size: 1.25rem;
+}
+.u-block-6cb4-22 {
+  box-shadow: 2px 2px 8px 0 rgba(128,128,128,1);
+}
+.u-header .u-text-1 {
+  margin: -36px 997px 38px 64px;
+}
+@media (max-width: 1199px) {
+  .u-header .u-image-1 {
+    width: 64px;
+    height: 32px;
+  }
+  .u-header .u-menu-1 {
+    width: auto;
+  }
+  .u-header .u-text-1 {
+    margin-right: 861px;
+  }
+}
+@media (max-width: 991px) {
+  .u-header .u-text-1 {
+    margin-right: 641px;
+  }
+}
+@media (max-width: 767px) {
+  .u-header .u-image-1 {
+    width: auto;
+  }
+  .u-header .u-text-1 {
+    margin-right: 461px;
+  }
+}
+@media (max-width: 575px) {
+  .u-header .u-text-1 {
+    margin-right: 261px;
+  }
+}</style>
